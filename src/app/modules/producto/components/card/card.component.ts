2910ac62557ef32cea6485b76efe2b0a59ad7a9f
Maratct <marat2.0v2@gmail.com>
2024-09-04 10:46:38 -0300

    Creamos las directivas Input y Output | Conectamosla informacion transmitida entre el componente padre y el hijo | Mostramos la informacion en un carrusel y una notificacion

--- src/app/modules/producto/components/card/card.component.ts
+++ src/app/modules/producto/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Productos } from 'src/app/models/productos';
 import { CrudService } from 'src/app/modules/admin/services/crud.service';
 
@@ -18,6 +18,15 @@ export class CardComponent {
   // Variable para manejar estado de un modal
   modalVisible: boolean=false
 
+  // Booleano para manejar la visibilidad de "Ultima Compra"
+  compraVisible:boolean=false
+
+  // Directivas para comunicarse con el componente padre
+  @Input()productoReciente: string=''
+
+  // Output sera definido como un nuevo evento
+  @Output()productoAgregado=new EventEmitter<Productos>
+
   constructor(public servicioCrud:CrudService){}
 
   ngOnInit():void{
@@ -34,10 +43,11 @@ mostrarVer(info:Productos){
   // Guarda informacion de un producto elegido por el usuario
   this.productoSeleccionado=info;
 }
+agregarProducto(info:Productos){
+  this.productoAgregado.emit(info)
 
-
-
-
-
+    this.compraVisible=true
+  
+}
 
 }
--- src/app/modules/producto/pages/producto/producto.component.ts
+++ src/app/modules/producto/pages/producto/producto.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Productos } from 'src/app/models/productos';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-producto',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./producto.component.css']
 })
 export class ProductoComponent {
+
+// String que modificara el valor de @input en el componente hijo
+product:string=''
+
+// Coleccion de productos añadidos a la lista
+productosCarrusel:Productos[]=[]
+
+productoAnadido(producto:Productos){
+
+  // Modificar del valor de 'product'
+  this.product=`${producto.nombre}: $${producto.precio}`
+
+  try{
+    /* Agregamos la informacion por el 
+    parametro de la funcion a la coleccion
+    de carrusel 
+    */
+    this.productosCarrusel.push(producto)
+
+    Swal.fire({
+      title:'Bien',
+      text:'ha añadido este producto con exito',
+      icon:'info'
+
+    })
+  }catch(error){
+    Swal.fire({
+      title:'¡Oh no!',
+      text:'Ha ocurrido un error\n'+error,
+      icon:'error'
+
+    })
+  }
+}
 
 }
